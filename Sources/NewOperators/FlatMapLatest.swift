84bb0278570425a348fa1401af5c035ff9d7b716
Jason Fieldman <jason@fieldman.org>
2025-03-21 06:59:17 -0700

    more annotations

--- Sources/NewOperators/FlatMapLatest.swift
+++ Sources/NewOperators/FlatMapLatest.swift
@@ -6,6 +6,10 @@
 import Combine
 
 public extension Publisher {
+    /// Transforms each element of the publisher into a new publisher and emits elements from the latest inner publisher.
+    ///
+    /// - Parameter transform: A closure that takes an element of the current publisher and returns a new publisher.
+    /// - Returns: A `Publishers.SwitchToLatest` instance that emits elements from the latest inner publisher.
     func flatMapLatest<P: Publisher>(
         _ transform: @escaping (Output) -> P
     ) -> Publishers.SwitchToLatest<P, Publishers.Map<Self, P>> {
@@ -14,6 +18,10 @@ public extension Publisher {
 }
 
 public extension DeferredPublisherProtocol {
+    /// Transforms each element of the deferred publisher into a new publisher and emits elements from the latest inner publisher.
+    ///
+    /// - Parameter transform: A closure that takes an element of the wrapped publisher and returns a new publisher.
+    /// - Returns: A `Deferred<Publishers.SwitchToLatest>` instance that emits elements from the latest inner publisher.
     @_disfavoredOverload
     func flatMapLatest<P: Publisher>(
         _ transform: @escaping (WrappedPublisher.Output) -> P
--- Sources/NewOperators/IgnoreFailure.swift
+++ Sources/NewOperators/IgnoreFailure.swift
@@ -6,7 +6,9 @@
 import Combine
 
 public extension Publisher {
-    /// Convenience wrapper to ignore failures.
+    /// Convenience wrapper to ignore failures by replacing them with an empty publisher.
+    ///
+    /// - Returns: A `Publishers.Catch<Self, Empty<Output, Failure>>` instance that ignores failures.
     func ignoreFailure() -> Publishers.Catch<Self, Empty<Output, Failure>> {
         self.catch { _ in
             Empty(outputType: Output.self, failureType: Failure.self)
@@ -15,6 +17,9 @@ public extension Publisher {
 }
 
 public extension DeferredPublisherProtocol {
+    /// Convenience wrapper to ignore failures for a deferred publisher by replacing them with an empty publisher.
+    ///
+    /// - Returns: A `Deferred<Publishers.Catch<WrappedPublisher, Empty<Output, Failure>>>` instance that ignores failures.
     @_disfavoredOverload
     func ignoreFailure() -> Deferred<Publishers.Catch<WrappedPublisher, Empty<Output, Failure>>> where WrappedPublisher: Publisher, WrappedPublisher.Failure == Failure {
         deferredLift { $0.ignoreFailure() }
