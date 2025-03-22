e9eb0f15325ab884cfb1dd11f20ffe87cc9463b6
Jason Fieldman <jason@fieldman.org>
2025-03-22 07:02:40 -0700

    attemptmap test

---
+++ Tests/CombineExTests/AttemptMapTests.swift
@@ -0,0 +1,65 @@
+//
+//  AttemptMapTests.swift
+//  Copyright © 2025 Jason Fieldman.
+//
+
+@testable import CombineEx
+import XCTest
+
+/// A generic error enum we can use for these test cases
+private enum TestError: Error {
+    case error1
+}
+
+final class AttemptMapTests: XCTestCase {
+    func testAttemptMap_TransformsOutputSuccessfully() {
+        let publisher = PassthroughSubject<Int, TestError>()
+        var results: [String?] = []
+
+        publisher
+            .attemptMap { value -> Result<String, TestError> in
+                .success("\(value)")
+            }
+            .handleValue {
+                results.append($0)
+            }
+            .handleCompletion {
+                if case .finished = $0 {
+                    results.append(nil)
+                }
+            }
+            .sink(duringLifetimeOf: self)
+
+        publisher.send(1)
+        publisher.send(2)
+        publisher.send(completion: .finished)
+
+        XCTAssertEqual(results, ["1", "2", nil])
+    }
+
+    func testAttemptMap_TransformsOutputWithError() {
+        let publisher = PassthroughSubject<Int, TestError>()
+        var results: [String?] = []
+
+        publisher
+            .attemptMap { value -> Result<String, TestError> in
+                if value == 2 { return .failure(TestError.error1) }
+                return .success("\(value)")
+            }
+            .handleValue {
+                results.append($0)
+            }
+            .handleCompletion {
+                if case .failure = $0 {
+                    results.append("fail")
+                }
+            }
+            .sink(duringLifetimeOf: self)
+
+        publisher.send(1)
+        publisher.send(2)
+        publisher.send(completion: .finished)
+
+        XCTAssertEqual(results, ["1", "fail"])
+    }
+}
