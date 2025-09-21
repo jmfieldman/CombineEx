d586ebe5f0d27c621e8ee107bbe12ee497090d5c
Jason Fieldman <jason@fieldman.org>
2025-09-21 08:01:17 -0700

    map void

---
+++ Sources/NewOperators/MapVoid.swift
@@ -0,0 +1,37 @@
+//
+//  MapVoid.swift
+//  Copyright © 2025 Jason Fieldman.
+//
+
+import Combine
+
+public extension Publisher {
+    /// Maps the output of this publisher to `Void`, discarding the original value.
+    ///
+    /// - Returns: A publisher that emits `Void` values.
+    func mapVoid() -> Publishers.Map<Self, Void> {
+        map { _ in () }
+    }
+}
+
+public extension DeferredPublisherProtocol {
+    /// Maps the output of this deferred publisher to `Void`, discarding the original value.
+    ///
+    /// - Returns: A deferred publisher that emits `Void` values.
+    @_disfavoredOverload
+    func mapVoid() -> Deferred<Publishers.Map<WrappedPublisher, Void>> where WrappedPublisher: Publisher {
+        deferredLift { $0.mapVoid() }
+    }
+}
+
+public extension DeferredFuture {
+    /// Maps the output of this deferred future to `Void`, discarding the original value.
+    ///
+    /// - Returns: A deferred future that produces `Void` values.
+    @_disfavoredOverload
+    func mapVoid() -> DeferredFuture<Void, Failure> {
+        futureLiftOutput { _, innerPromise in
+            innerPromise(.success(()))
+        }
+    }
+}
