38db3150bc1acb0b065c26db6c2b6a084fb42e6f
Jason Fieldman <jason@fieldman.org>
2025-10-03 17:38:22 -0700

    async on main

--- Sources/NewOperators/AsyncFuture.swift
+++ Sources/NewOperators/AsyncFuture.swift
@@ -45,11 +45,59 @@ public extension DeferredFutureProtocol {
     }
 
     /// Allows a DeferredFuture to be used in the context of structured concurrency
-    /// as a typical async function. use `asyncThrowing` for futures that can return
-    /// a non-Never failure.
+    /// as a typical async function. use `asyncThrowing` instead for futures that can
+    /// return a non-Never failure.
     ///
     /// This function captures the future until it emits an output or failure.
     func async() async -> Output where Failure == Never {
+        await withUnsafeContinuation { [self] continuation in
+            var futureBox: FutureBox? = FutureBox(self)
+            futureBox?.cancellable = sink(receiveValue: { value in
+                continuation.resume(returning: value)
+                futureBox = nil
+            })
+        }
+    }
+    
+    /// Allows a DeferredFuture to be used in the context of structured concurrency
+    /// as a typical async function. It will either return its Output or throw its Failure.
+    ///
+    /// This version is meant to be used with main-thread isolated tasks.
+    ///
+    /// This function captures the future until it emits an output or failure.
+    @MainActor func asyncThrowingOnMain() async throws(Failure) -> Output {
+        let result: Result<Output, Failure> = await withUnsafeContinuation { [self] continuation in
+            var futureBox: FutureBox? = FutureBox(self)
+            futureBox?.cancellable = sink(receiveCompletion: { result in
+                switch result {
+                case let .failure(error):
+                    continuation.resume(returning: .failure(error))
+                case .finished:
+                    break
+                }
+                futureBox = nil
+            }, receiveValue: { value in
+                continuation.resume(returning: .success(value))
+                futureBox = nil
+            })
+        }
+
+        switch result {
+        case let .success(output):
+            return output
+        case let .failure(failure):
+            throw failure
+        }
+    }
+    
+    /// Allows a DeferredFuture to be used in the context of structured concurrency
+    /// as a typical async function. use `asyncThrowingOnMain` for futures that can return
+    /// a non-Never failure.
+    ///
+    /// This version is meant to be used with main-thread isolated tasks.
+    ///
+    /// This function captures the future until it emits an output or failure.
+    @MainActor func asyncOnMain() async -> Output where Failure == Never {
         await withUnsafeContinuation { [self] continuation in
             var futureBox: FutureBox? = FutureBox(self)
             futureBox?.cancellable = sink(receiveValue: { value in
